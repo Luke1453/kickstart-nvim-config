065ff2bb2a184fced3f19d469a211128d10474d2
Luke1453 <lukjut@gmail.com>
2024-01-22 17:09:16 +0200

    added go plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,13 +1,24 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
---
+
 -- See the kickstart.nvim README for more information
 return {
+  --{
+  --"jose-elias-alvarez/null-ls.nvim",
+  --ft = "go",
+  --opts = function ()
+  --return require "custom.plugins.configs.go-null-ls"
+  --end,
+  --},
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "olexsmir/gopher.nvim",
     ft = "go",
-    opts = function ()
-      return require "custom.plugins.configs.go-null-ls"
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
     end
-  }
-}
+  },
+};
+
